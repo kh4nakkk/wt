f90bad3c09fefc9d4b36139a75f36d849428484f
Khanak Korotania <khanakkoro@gmail.com>
2024-11-11 21:32:11 +0530

    Add files via upload

---
+++ j2.js
@@ -0,0 +1,21 @@
+const dynamicText = document.querySelector("h1 span");
+const words = ["About Us"];
+
+// Variables to track the position and deletion status of the word
+let wordIndex = 0;
+let charIndex = 1;
+let isDeleting = false;
+
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChar = currentWord.substring(0, charIndex);
+    dynamicText.textContent = currentChar;
+    dynamicText.classList.add("stop-blinking");
+
+    if (!isDeleting && charIndex < currentWord.length) {
+        // If condition is true, type the next character
+        charIndex++;
+        setTimeout(typeEffect, 200);
+    } 
+}
+typeEffect();
